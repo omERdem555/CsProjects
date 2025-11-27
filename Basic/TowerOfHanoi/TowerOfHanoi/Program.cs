46f2ec995ead021f2c876b7a7fba40070cde89c5
Omerdem <152390583+omERdem555@users.noreply.github.com>
2025-11-27 10:10:39 +0300

    Tower Of Hanoi
    The Tower of Hanoi is a mathematical game or puzzle consisting of three rods and a number of disks of various diameters, which can slide onto any rod. The puzzle begins with the disks stacked on one rod in order of decreasing size, the smallest at the top, thus approximating a conical shape

---
+++ Basic/TowerOfHanoi/TowerOfHanoi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class TowerOfHanoi
+{
+    static void Main(string[] args)
+    {
+        int diskSayisi = 3; // Taşıyacağımız disk sayısı
+        Console.Write("Hanoi Kulesi için disk sayısını girin:");
+        if (int.TryParse(Console.ReadLine(), out int input))
+        {
+            diskSayisi = input;
+        }
+
+        HanoiKulesi(diskSayisi, 'A', 'C', 'B');
+    }
+
+    static void HanoiKulesi(int n, char kaynak, char hedef, char ara)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Disk 1 {kaynak} kulesinden {hedef} kulesine taşındı.");
+            return;
+        }
+
+        HanoiKulesi(n - 1, kaynak, ara, hedef);
+        Console.WriteLine($"Disk {n} {kaynak} kulesinden {hedef} kulesine taşındı.");
+        HanoiKulesi(n - 1, ara, hedef, kaynak);
+    }
+}
