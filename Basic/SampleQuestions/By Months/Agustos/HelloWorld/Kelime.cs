543c0fa4b15c3455ea31f1a8d3eade5bae97435f
Omerdem <152390583+omERdem555@users.noreply.github.com>
2025-11-27 10:21:57 +0300

    Ağustos

---
+++ Basic/SampleQuestions/By Months/Agustos/HelloWorld/Kelime.cs
@@ -0,0 +1,42 @@
+﻿internal class Kelime
+{
+    public string ArananMetin { get; set; }
+
+    public void Yaz()
+    {
+        string bulunan = new string(' ', ArananMetin.Length);
+
+        for (int index = 0; index < ArananMetin.Length; index++)
+        {
+            char hedef = ArananMetin[index];
+
+            // Harf değilse direkt ekle
+            if (!char.IsLetter(hedef))
+            {
+                bulunan = bulunan.Remove(index, 1).Insert(index, hedef.ToString());
+                Console.WriteLine(bulunan);
+                continue;
+            }
+
+            // Unicode aralığında karakterleri dene (Türkçe dahil)
+            for (char c = '\u0020'; c <= '\u05FF'; c++)
+            {
+                string temp = bulunan.Remove(index, 1).Insert(index, c.ToString());
+                Console.WriteLine(temp);
+                Thread.Sleep(5);
+
+                if (hedef == c)
+                {
+                    bulunan = temp;
+                    break;
+                }
+            }
+        }
+
+        // Sonuç rengini değiştir
+        Console.WriteLine("\a");
+        Console.ForegroundColor = ConsoleColor.Green; // İstediğin renk
+        Console.WriteLine(bulunan);
+        Console.ResetColor();
+    }
+}
---
+++ Basic/SampleQuestions/By Months/Agustos/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8; // Türkçe karakterler için
+        Console.Write("Metin girin: ");
+        var metin = "İstemiyorsanız Gideyim";
+
+        Kelime k = new Kelime
+        {
+            ArananMetin = metin
+        };
+        k.Yaz();
+    }
+}
