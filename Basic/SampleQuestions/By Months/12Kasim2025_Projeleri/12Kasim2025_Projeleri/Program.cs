52e1d73e30ab471f9a9ccf4dfcd059f37fd22f15
Omerdem <152390583+omERdem555@users.noreply.github.com>
2025-11-27 10:21:21 +0300

    Kasım

---
+++ Basic/SampleQuestions/By Months/12Kasim2025_Projeleri/12Kasim2025_Projeleri/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {/*
+        int i = 2;
+        Console.Write(++i);
+        Console.Write(i++);
+        Console.Write(i);
+        Console.WriteLine("\n\n");
+
+        int x = 15, y = 5, result;
+
+        result = x & y;
+        Console.WriteLine(result);
+        result = x | y;
+        Console.WriteLine(result);
+        result = x ^ y;
+        Console.WriteLine(result);
+        result = x << 2;
+        Console.WriteLine(result);
+        result = x >> 2;
+        Console.WriteLine(result);*/
+
+        byte x = 15, y = 254;
+        Console.WriteLine(Convert.ToString(x, toBase: 2));
+        Console.WriteLine(Convert.ToString(y, toBase: 2));
+
+        var result = x & y;
+        Console.WriteLine(result);
+
+        result = result - (x | y);
+        Console.WriteLine(--result);
+
+        result = result - x ^ y;
+        Console.WriteLine(result++);
+
+        result = (result + x) << 2; 
+        Console.WriteLine("result");
+
+        result = result + x >> 2;
+        Console.WriteLine(result);
+    }
+}
