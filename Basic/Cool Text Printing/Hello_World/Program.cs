305d17dc48bf9fecf374014eb1b34a454a66c6d3
Omerdem <152390583+omERdem555@users.noreply.github.com>
2025-11-27 10:31:20 +0300

    Cool Text Printing
    Code that prints an entered text on the screen by searching for letters and scrolling down.

---
+++ Basic/Cool Text Printing/Hello_World/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Hello_World
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Hangi Metni Bastırmak İstiyorsun : ");
+        string Metin = Convert.ToString(Console.ReadLine());
+        char[] ArananHarfler = new char[Metin.Length];
+        for (int j = 0; j < Metin.Length; j++)
+        {
+            for (int i = 32; i <= 122; i++)
+            {
+                foreach (char c in ArananHarfler)
+                    Console.Write(c);
+                Console.WriteLine((char)i);
+                if (Metin[j] == (char)i)
+                {
+                    ArananHarfler[j] = (char)i;
+                    break;
+                }
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
